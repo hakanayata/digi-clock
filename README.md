e164e3d4ccd3d5fbabed9457ced186ca20e44768
Hakan <s.hakanayata@gmail.com>
2023-02-23 22:55:57 +0100

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# Project : Digital Clock
+
+## Demo
+
+- [Netlify](https://digi-clock-hakan.netlify.app)
+
+## Description
+A website application that shows the current time and also comprises a stopwatch feature.
+
+<br>
+
+## Preview
+
+![Project Snapshot](./digi-clock.gif)
+
+<br>
+
+## Tools
+
+- HTML 
+
+- CSS (Bootstrap v5.3)
+
+- JavaScript (`setInterval()`, `clearInterval()` )
+
+- GIT & GitHub
+
+- Netlify
---
+++ script.js
@@ -0,0 +1,100 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    //#region DOM elements
+    const hourEl = document.getElementById("hours")
+    const minEl = document.getElementById("minutes")
+    const secEl = document.getElementById("seconds")
+
+    const swHourEl = document.getElementById("sw-hours")
+    const swMinEl = document.getElementById("sw-minutes")
+    const swSecEl = document.getElementById("sw-seconds")
+
+    const startStopBtn = document.getElementById("start-stop")
+    const resetBtn = document.getElementById("reset")
+
+    //#endregion
+
+    function activateDisableResButton() {
+        if (!isStopwatchOn) {
+            resetBtn.removeAttribute("disabled")
+        } else {
+            resetBtn.setAttribute("disabled", "true")
+        }
+    }
+
+    //#region Digital Clock
+    setInterval(() => {
+        // variables
+        const now = new Date()
+        const currentHours = String(now.getHours()).padStart(2, '0')
+        const currentMinutes = String(now.getMinutes()).padStart(2, '0')
+        const currentSeconds = String(now.getSeconds()).padStart(2, '0')
+
+        hourEl.textContent = currentHours
+        minEl.textContent = currentMinutes
+        secEl.textContent = currentSeconds
+
+    }, 1000);
+    //#endregion
+
+    //#region STOPWATCH
+    let isStopwatchOn = false
+    let intervalID = null
+    let hours = 0
+    let minutes = 0
+    let seconds = 0
+    let stats = 0
+
+    startStopBtn.addEventListener("click", () => {
+        isStopwatchOn = !isStopwatchOn
+        activateDisableResButton()
+
+        if (isStopwatchOn) {
+            clearInterval(intervalID)
+            startStopBtn.textContent = "Stop"
+            startStopBtn.classList.remove("btn-success")
+            startStopBtn.classList.add("btn-danger")
+            intervalID = setInterval(stopwatch, 1000);
+        } else {
+            startStopBtn.textContent = "Start"
+            startStopBtn.classList.remove("btn-danger")
+            startStopBtn.classList.add("btn-success")
+            clearInterval(intervalID)
+        }
+    })
+
+    resetBtn.addEventListener("click", () => {
+        if (!isStopwatchOn) {
+            swHourEl.textContent = "00"
+            swMinEl.textContent = "00"
+            swSecEl.textContent = "00"
+            clearInterval(intervalID)
+        }
+    })
+
+    function stopwatch() {
+        seconds += 1
+        if (seconds > 59) {
+            minutes += 1
+            seconds = 0
+        }
+
+        if (minutes > 59) {
+            hours += 1
+            minutes = 0
+        }
+
+        if (hours > 23) {
+            hours = 0
+            minutes = 0
+            seconds = 0
+        }
+
+        swSecEl.textContent = String(seconds).padStart(2, '0')
+        swMinEl.textContent = String(minutes).padStart(2, '0')
+        swHourEl.textContent = String(hours).padStart(2, '0')
+
+    }
+    //#endregion
+
+})
